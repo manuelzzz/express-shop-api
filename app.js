8f0ee30579a0ec783435752b5ba97bd6167bbfd1
Manel <manuelss.23.bil@gmail.com>
2023-11-28 11:33:04 -0300

    feat: handling not found error

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const productRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
 
+app.use(morgan("dev"));
+
 app.use("/products", productRoutes);
 app.use("/orders", ordersRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message,
+        },
+    });
+});
 
 module.exports = app;
