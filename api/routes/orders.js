b03cb3f907fd6c8b723da164fc2c6173dd2ee10c
Manel <manuelss.23.bil@gmail.com>
2023-12-01 22:57:53 -0300

    feat: handling the get orders requests fix: improoving the post order response

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,9 +5,41 @@ const mongoose = require("mongoose");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.status(200).json({
-        message: "Orders were fetched",
-    });
+    Order.find()
+        .select("product quantity _id")
+        .exec()
+        .then((docs) => {
+            const response = {
+                count: docs.length,
+                orders: docs.map((doc) => {
+                    return {
+                        _id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        requests: [
+                            {
+                                type: "GET",
+                                description: "Get only this order",
+                                url: "http://localhost:3000/orders/" + doc._id,
+                            },
+                            {
+                                type: "GET",
+                                description: "Get the product",
+                                productUrl:
+                                    "http://localhost:3000/products" +
+                                    doc.product,
+                            },
+                        ],
+                    };
+                }),
+            };
+
+            res.status(200).json(response);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json({ error: err });
+        });
 });
 
 router.post("/", (req, res, next) => {
@@ -20,7 +52,27 @@ router.post("/", (req, res, next) => {
     order
         .save()
         .then((result) => {
-            res.status(201).json(result);
+            res.status(201).json({
+                createdOrder: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity,
+                    requests: [
+                        {
+                            type: "GET",
+                            description: "Get all orders",
+                            url: "http://localhost:3000/orders",
+                        },
+                        {
+                            type: "GET",
+                            description: "Get the product",
+                            url:
+                                "http://localhost:3000/products" +
+                                result.product,
+                        },
+                    ],
+                },
+            });
         })
         .catch((err) => {
             console.error(err);
